205c137a26788cd413df97195b42eaab0381db9c
Daniel Martinez <dmartinez123456@cesde.net>
2026-03-03 00:03:38 -0500

    logo de Creciendo Juntos añadido

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 const Navbar = () => {
   return (
-    <nav>
-      <h1>Creciendo Juntos</h1>
+    <nav className="navbar">
+      <div className="brand">
+        <img className="brand-logo" src="/public/LOGO CRECIENDO JUNTOS.png" alt="Logo de Creciendo Juntos" />
+        <span className="brand-name">Creciendo Juntos</span>
+      </div>
     </nav>
   );
 };
